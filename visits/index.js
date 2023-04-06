27073073ce2a76e121f39913913e9d09280591ec
Prakash, Deva <Deva.Prakash3@T-Mobile.com>
2023-04-06 18:58:39 +0530

    first

---
+++ visits/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const redis = require('redis');
+const process = require('process');
+
+const app = express();
+const client = redis.createClient({
+  host: 'redis-server',
+  port: 6379
+});
+client.set('visits', 0);
+
+app.get('/', (req, res) => {
+  client.get('visits', (err, visits) => {
+    client.set('visits', parseInt(visits) + 1);
+    res.send('Number of visits is ' + visits);
+  });
+});
+
+app.get('/exit', (req, res) => {
+  process.exit(0);
+});
+
+app.listen(8081, () => {
+  console.log('Listening on port 8081');
+});
